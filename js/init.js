f082fe78180fe45e668383477dd240f1979f460b
Konstantin <Ramkirvu@gmail.com>
2017-06-06 09:55:20 +0300

    backup

--- js/init.js
+++ js/init.js
@@ -5,3 +5,9 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(document).ready(function() {
+    $('select').material_select();
+});
+
+$('.carousel.carousel-slider').carousel({fullWidth: true});
