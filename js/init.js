83e4538f5813023cd77fc6a124d01eebc02a9ada
Konstantin <Ramkirvu@gmail.com>
2017-06-07 16:09:58 +0300

    Tripfinder v.0.0.2

--- js/init.js
+++ js/init.js
@@ -1,21 +1,37 @@
-(function($){
-  $(function(){
 
-    $('.button-collapse').sideNav();
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+//MOBILE SIDEBAR
+$(document).ready(function($){
+    $(function() {
+        $(".button-collapse").sideNav();
+    });
+});
 
+//OFFSET SHOW
+var options = [
+    {   selector: '#brand', offset: 50,
+        callback: function(el) {
+            Materialize.fadeInImage($(el));
+        }, }
+];
+Materialize.scrollFire(options);
+
+var options = [
+    {   selector: '#brand-name', offset: 100,
+        callback: function(el) {
+            Materialize.fadeInImage($(el));
+        }, }
+];
+Materialize.scrollFire(options);
+
+//CAROUSEL FOTO GALLERY
+$('.carousel.carousel-slider').carousel({fullWidth: true});
+
+//
 $(document).ready(function() {
     $('select').material_select();
 });
 
-$('.carousel.carousel-slider').carousel({fullWidth: true});
 
-// var options = [
-//     {class: '.cir', offset: 200, callback: customCallbackFunc } },
-//     {selector: '.cir', offset: 200, callback: function() {
-//     customCallbackFunc();
-// } },
-// ];
-// Materialize.scrollFire(options);
+
+
